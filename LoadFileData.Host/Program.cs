3d1f67cc768c99919ab656711b60fc43f00e5757
Warrenn <warrenn@busyweb.co.za>
2015-08-30 22:33:58 +0200

    OWIN Host with toaster

--- LoadFileData.Host/Program.cs
+++ LoadFileData.Host/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using LoadFileData.Web;
 using Microsoft.Owin.Hosting;
 
@@ -11,15 +16,74 @@ namespace LoadFileData.Host
 {
     class Program
     {
-        static void Main(string[] args)
+        private static ContextMenu menu;
+        private static MenuItem mnuExit;
+        private static MenuItem mnuShow;
+        private static NotifyIcon notificationIcon;
+        private static string url;
+
+        private static void Main(string[] args)
         {
-            var url = ConfigurationManager.AppSettings["host:url"];
-            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data"));
+
+            url = ConfigurationManager.AppSettings["host:url"];
+            AppDomain.CurrentDomain.SetData("DataDirectory",
+                Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data"));
+            AppDomain.CurrentDomain.ProcessExit += Application_ApplicationExit;
+
+            var mainTask = Task.Run(() =>
+            {
+                menu = new ContextMenu();
+                mnuExit = new MenuItem("Exit");
+                mnuShow = new MenuItem("Show");
+                menu.MenuItems.Add(0, mnuExit);
+                menu.MenuItems.Add(1, mnuShow);
+
+                notificationIcon = new NotifyIcon
+                {
+                    Icon = Properties.Resources.LoadFileDataIcon,
+                    ContextMenu = menu,
+                    Text = "LoadFileData"
+                };
+
+                mnuExit.Click += MnuExit_Click;
+                mnuShow.Click += MnuShow_Click;
+
+                notificationIcon.Visible = true;
+                Application.Run();
+            });
+
             using (WebApp.Start<Startup>(url))
             {
-                Console.WriteLine("Press [enter] to quit...");
-                Console.ReadLine();
+                MnuShow_Click(mnuShow, EventArgs.Empty);
+                mainTask.Wait();
             }
+        }
+
+        private static void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            AppDomain.CurrentDomain.ProcessExit -= Application_ApplicationExit;
+            mnuExit.Click -= MnuExit_Click;
+            mnuShow.Click -= MnuShow_Click;
+            notificationIcon.Visible = false;
+            notificationIcon.Dispose();
+            notificationIcon = null;
+        }
+
+        private static void MnuShow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch (Win32Exception)
+            {
+                Process.Start("IExplore.exe", url);
+            }
+        }
+
+        private static void MnuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
